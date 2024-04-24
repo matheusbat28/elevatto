4c6fde393086cd1e458a03452e626aa36ecbac32
matheusbat28 <matheusarrow28@gmail.com>
2024-04-23 21:13:54 -0300

    react-bootstrap

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -1,20 +1,36 @@
 import React from "react";
-import './login.css';
+import styled from 'styled-components';
 import fundo from '../../assets/fundo.jpg';
 import { BsPerson } from "react-icons/bs";
 
 export default function Login() {
+  const Img = styled.img`
+    width: 100%;
+    height: 100vh;
+    position: absolute;
+    z-index: -1;
+    `;
+
+  const FormLogin = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100vh;
+    position: absolute;
+    z-index: 1;
+    `;
+
   return (
     <>
-      <img src={fundo} alt="fundo" />
-      <form id="formLogin" >
-        <div className="d-flex">
-          <label htmlFor="email">
-            <BsPerson />
-          </label>
-          <input type="email" id="email" name="email" required />
-        </div>
-      </form>
+      <Img src={fundo} alt="fundo" />
+      <FormLogin>
+        <BsPerson size={100} color="white" />
+        <input type="text" placeholder="Usuário" />
+        <input type="password" placeholder="Senha" />
+        <button type="submit">Entrar</button>
+      </FormLogin>
     </>
   )
 }
