2b554d44009c5890eb045cfdc79a26ad9529007d
Marcos Henrique Warmling <marcoswarmling15@gmail.com>
2024-04-28 21:55:17 -0300

    fix/footer

--- frontend/src/components/home/Footer.jsx
+++ frontend/src/components/home/Footer.jsx
@@ -1,26 +1,33 @@
 import React from 'react'
 import logo from '../../images/logo.png'
+import { FaGoogle, FaWhatsapp  } from "react-icons/fa";
+import { IoLogoInstagram } from "react-icons/io";
 
 export default function Footer() {
-  return (
-    <div className="footer row col-12">
-      <div className="col-3 ">
-        <img
-            src={logo}
-            alt="pin"
-           
-        />
-      </div>
-      <div className="col-7 align-self-center ">
-        <h4>Elevatto Imobiliária</h4>
-        <p>CNPJ - 00.000.000/0000-00</p>
-        <p>Endereço: Rua dos Bobos, nº 0</p>
-      </div>
-      <div className="col-2 align-self-center">
-        <p>contato@gmail.com</p>
-        <p>(00) 0000-0000</p>
-        <p>@Imobiliária Elevatto</p>
-      </div>
-    </div>
-  );
+    return (
+        <div className="footer d-flex col-12">
+          <div className="col-4 text-center">
+            <img
+                src={logo}
+                alt="pin"
+               
+            />
+          </div>
+          <div className="col-4 align-self-center text-center">
+            <p>Elevatto Imobiliária</p>
+            <p>CNPJ - 00.000.000/0000-00</p>
+            <p>Endereço: Rua dos Bobos, nº 0</p>
+          </div>
+          <div></div>
+          <div className="col-4 align-self-center text-center">
+            <p>
+            <FaGoogle /> contato@gmail.com</p>
+            <p>
+            <FaWhatsapp /> (00) 0000-0000</p>
+            <p><IoLogoInstagram /> @Imobiliária Elevatto</p>
+         
+          </div>
+          
+        </div>
+      );
 }
