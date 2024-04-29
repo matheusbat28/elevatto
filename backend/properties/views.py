7d64eabe2068e8f00ab8fe18c54779bbb7df3abb
matheusbat28 <matheusarrow28@gmail.com>
2024-04-28 21:13:59 -0300

    listar propriedade para todos

--- backend/properties/views.py
+++ backend/properties/views.py
@@ -8,3 +8,11 @@ class PropertiesViewSet(viewsets.ModelViewSet):
     queryset = Properties.objects.all()
     serializer_class = PropertiesSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        if self.action == 'list':
+            self.permission_classes = []
+        else:
+            self.permission_classes = [IsAuthenticated]
+        
+        return super(self.__class__, self).get_permissions()
