d8fa5af6c08587423e42cf17f02c5bbea1647990
matheusbat28 <matheusarrow28@gmail.com>
2024-05-01 19:42:29 -0300

    ajuste no erro

--- frontend/src/controls/functions.ts
+++ frontend/src/controls/functions.ts
@@ -1,5 +1,5 @@
 export function requestsError(error: any) {
-    const listErrors = ['detail', 'non_field_errors', 'username', 'password'];
+    const listErrors = ['detail', 'non_field_errors', 'username', 'password', 'foto', 'email', 'first_name', 'last_name', 'title', 'description', 'bedrooms', 'bathrooms', 'parking', 'area', 'photos', 'price'];
 
     for (let i = 0; i < listErrors.length; i++) {
         if (error[listErrors[i]]) {
