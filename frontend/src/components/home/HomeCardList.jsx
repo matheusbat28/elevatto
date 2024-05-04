e4554a487dfd0267adb3eb63e25df3e7bf650252
matheusbat28 <matheusarrow28@gmail.com>
2024-05-04 16:12:54 -0300

    get das propriedade no home

--- frontend/src/components/home/HomeCardList.jsx
+++ frontend/src/components/home/HomeCardList.jsx
@@ -4,7 +4,7 @@ import { exampleData } from "./exempleData";
 import HomeModal from "../modals/HomeModal";
 import { getProperties, getPhotos } from "../../controls/requests";
 
-export default function HomeCardList() {
+export default function HomeCardList(props) {
   const [modalShow, setModalShow] = useState(false);
   const [selectedHome, setSelectedHome] = useState(null);
   const [type, setType] = useState("display")
@@ -13,82 +13,73 @@ export default function HomeCardList() {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    getProperties().then((data) => {
-      setProperties(data);
-      let firstImages = []
-      data.forEach(element => {
-   
-         (element.photos && element.photos[0]) &&
-          firstImages.push(element.photos[0])
-      });
-      getPhotos(
-        firstImages
-      ).then((photosArray) => {
-        setPhotosArray(photosArray);
-        console.log(photosArray);
-
-      })
+    setProperties(props.properties);
+    let firstImages = []
+    props.properties.forEach(element => {
+      (element.photos && element.photos[0]) &&
+        firstImages.push(element.photos[0])
     });
-      
+    getPhotos(
+      firstImages
+    ).then((photosArray) => {
+      setPhotosArray(photosArray);
+    });
+  }, [props.properties]);
 
-  }, []);
-
-  function getPhotoById(id){
+  function getPhotoById(id) {
     return "http://localhost:8000" + photosArray.find(photo => photo.id === id)?.foto
   }
 
 
-
-  
   return (
     <>
-    <div className="col-12 mt-4 text-right pe-4">
-      { localStorage.getItem("access") &&
-    <button className="btn btn-primary " onClick={() =>{
-      setSelectedHome(
-        { title: "", description: "", price: "", location: "", images: []}
-      );
-      setType("edit")
-      setModalShow(true);
+      <div className="col-12 mt-4 text-right pe-4">
+        {localStorage.getItem("access") &&
+          <button className="btn btn-primary " onClick={() => {
+            setSelectedHome(
+              { title: "", description: "", price: "", location: "", images: [] }
+            );
+            setType("edit")
+            setModalShow(true);
 
-    }}>Criar anuncio</button>}
-    </div>
-    <div
-      className="
+          }}>Criar anuncio</button>}
+      </div>
+      <div
+        className="
     home-card-box 
     "
-    >
-     
-      {(!loading && properties[0]) ? properties.map((item, index) => {
-        return (
-          <div
-          key={index}
-          >
-          <HomeCard
-            key={item.id}
-            {...item}
-            item={item}
-            setSelectedHome={setSelectedHome}
-            setModalShow={setModalShow}
-            setType={setType}
-            photo={
-              item.photos[0] ?   getPhotoById(item.photos[0]) : "https://via.placeholder.com/150"
-            }
-          />
+      >
+
+        {(!loading && properties[0]) ? properties.map((item, index) => {
+          return (
+            <div
+              key={index}
+            >
+              <HomeCard
+                key={item.id}
+                {...item}
+                item={item}
+                setSelectedHome={setSelectedHome}
+                setModalShow={setModalShow}
+                setType={setType}
+                photo={
+                  item.photos[0] ? getPhotoById(item.photos[0]) : "https://via.placeholder.com/150"
+                }
+              />
+            </div>
+          );
+        }) : (
+          <div className="col-12 mt-4">
+            <h4 className="text-center text-black">Nenhum anúncio encontrado</h4>
           </div>
-        );
-      }): (
-        <div className="col-12 mt-4">
-          <h4 className="text-center text-black">Nenhum anúncio encontrado</h4>
-        </div>
-      )}
-      
-    {  selectedHome && <HomeModal show={modalShow} onHide={() => setModalShow(false)} 
-     type={type}
-        {...selectedHome}
-      />}
-    </div>
+        )}
+
+        {selectedHome && <HomeModal show={modalShow} onHide={() => setModalShow(false)}
+          type={type}
+          {...selectedHome}
+        />}
+      </div>
     </>
   );
- 
+
 }
