542269c89e74ccfd651526a1acabfd7df0345dd8
matheusbat28 <matheusarrow28@gmail.com>
2024-05-04 00:12:04 -0300

    ajuste navbar

--- frontend/src/components/home/NavBar.jsx
+++ frontend/src/components/home/NavBar.jsx
@@ -1,22 +1,25 @@
-import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import LOGO from "../../assets/logoVetor.svg";
 import "../home/App.css";
 import { FaSearch } from "react-icons/fa";
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { getUserLogged } from "../../controls/requests";
-import "../home/App.css";
+import { useState, useEffect } from "react";
+import { Dropdown } from 'react-bootstrap';
+import '../../index.css'
 
 const NavBar = () => {
   const [inputValue, setInputValue] = useState("");
-  const [user, setUser] = useState(null);
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 575.98);
 
-  React.useEffect(() => {
-    getUserLogged()
-      .then((response) => {
-        setUser(response);
-      })
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 575.98);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
   }, []);
 
   return (
@@ -53,10 +56,66 @@ const NavBar = () => {
           <Nav.Link href="#propriedade" className={" navbar-link"}>
             Tipo de propriedade
           </Nav.Link>
-          <Nav.Link href="/login" className={"navbar-link"}>
-            Olá {user ? user.username : "Visitante"}
+          <Nav.Link href="Usuario" className={"navbar-link"}>
+            Olá, visitante
           </Nav.Link>
         </Nav>
+        {isSmallScreen && (
+          <>
+            <Dropdown className="nav-item dropdown-container">
+               <Dropdown.Toggle className="nav-link" id="dropdown-basic" >
+                Locais
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu className="dropdown-menu">
+                <Dropdown.Item href="#">Local1</Dropdown.Item>
+                <Dropdown.Item href="#">Local1</Dropdown.Item>
+                <Dropdown.Item href="#">Local1</Dropdown.Item>
+                <Dropdown.Divider />
+              </Dropdown.Menu>
+            </Dropdown>
+
+            <Dropdown className="nav-item dropdown-container">
+              <Dropdown.Toggle className="nav-link" id="dropdown-basic">
+                Preços
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu className="dropdown-menu">
+                <Dropdown.Item href="#">10 real</Dropdown.Item>
+                <Dropdown.Item href="#">10 real</Dropdown.Item>
+                <Dropdown.Item href="#">10 real</Dropdown.Item>
+                <Dropdown.Divider />
+              </Dropdown.Menu>
+            </Dropdown>
+
+            <Dropdown className="nav-item dropdown-container">
+              <Dropdown.Toggle className="nav-link" id="dropdown-basic">
+                Quartos
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu className="dropdown-menu">
+                <Dropdown.Item href="#">1</Dropdown.Item>
+                <Dropdown.Item href="#">2</Dropdown.Item>
+                <Dropdown.Item href="#">3</Dropdown.Item>
+                <Dropdown.Divider />
+              </Dropdown.Menu>
+            </Dropdown>
+
+            <Dropdown className="nav-item dropdown-container">
+              <Dropdown.Toggle className="nav-link" id="dropdown-basic">
+                Mais
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu className="dropdown-menu">
+                <Dropdown.Item href="#">Action</Dropdown.Item>
+                <Dropdown.Item href="#">Another action</Dropdown.Item>
+                <Dropdown.Item href="#">Something else here</Dropdown.Item>
+                <Dropdown.Divider />
+              </Dropdown.Menu>
+            </Dropdown>
+            <button className="dropdown-container" style={{ margin: '0 12px', padding: '5px 20px', height: '40px', borderRadius: '5px', border: 'none', backgroundColor: '#FEBD59', color: 'white' }}> Buscar </button>
+          </>
+        )}
         <span className="navbar-text"></span>
       </Navbar.Collapse>
     </Navbar>
