29ffb92208c07aa6a965bc0bc13867b5916154f3
matheusbat28 <matheusarrow28@gmail.com>
2024-05-04 23:29:41 -0300

    inicio do filtros

--- frontend/src/components/home/filtersHome.tsx
+++ frontend/src/components/home/filtersHome.tsx
@@ -4,9 +4,41 @@ import "../home/App.css";
 
 export default function SubHeader(props: any) {
 
+    const [bathrooms, setBathrooms] = React.useState([]);
+    const [bedrooms, setBedrooms] = React.useState([]);
+    const [parking, setParking] = React.useState([]);
+    const [price, setPrice] = React.useState([]);
+
+
+
     React.useEffect(() => {
-        console.log(props.properties);
-    }, []);
+
+        props.properties.map((property: any) => {
+
+            if (!bathrooms.includes(property.bathrooms)) {
+                setBathrooms([...bathrooms, property.bathrooms])
+            }
+
+            if (!bedrooms.includes(property.bedrooms)) {
+                setBedrooms([...bedrooms, property.bedrooms])
+            }
+
+            if (!parking.includes(property.parking)) {
+                setParking([...parking, property.parking])
+            }
+
+            if (!price.includes(property.price)) {
+                setPrice([...price, property.price])
+            }
+
+
+
+        });
+
+
+
+    }, [props])
+
 
     return (
         <div style={{ display: 'flex', justifyContent: 'center', width: '100%', position: 'absolute', top: '0', left: '0', zIndex: 1 }}>
@@ -30,10 +62,9 @@ export default function SubHeader(props: any) {
                     </Dropdown.Toggle>
 
                     <Dropdown.Menu className="dropdown-menu">
-                        <Dropdown.Item href="#">10 real</Dropdown.Item>
-                        <Dropdown.Item href="#">10 real</Dropdown.Item>
-                        <Dropdown.Item href="#">10 real</Dropdown.Item>
-                        <Dropdown.Divider />
+                        {price.map((price: any) => {
+                            return <Dropdown.Item href="#">{price}</Dropdown.Item>
+                        })}
                     </Dropdown.Menu>
                 </Dropdown>
 
@@ -43,10 +74,9 @@ export default function SubHeader(props: any) {
                     </Dropdown.Toggle>
 
                     <Dropdown.Menu className="dropdown-menu">
-                        <Dropdown.Item href="#">1</Dropdown.Item>
-                        <Dropdown.Item href="#">2</Dropdown.Item>
-                        <Dropdown.Item href="#">3</Dropdown.Item>
-                        <Dropdown.Divider />
+                        {bedrooms.map((bedroom: any) => {
+                            return <Dropdown.Item href="#">{bedroom}</Dropdown.Item>
+                        })}
                     </Dropdown.Menu>
                 </Dropdown>
 
