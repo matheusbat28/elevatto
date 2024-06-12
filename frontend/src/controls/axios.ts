b80cc57776724a7e1acb1aa2aa8d6d41278aa047
matheusbat28 <matheusarrow28@gmail.com>
2024-06-12 00:40:05 -0300

    ajuste url

--- frontend/src/controls/axios.ts
+++ frontend/src/controls/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000/api/v1/",
+  baseURL: "https://imobiliariaelevatto.com//api/v1/",
 });
 
 export default instance;
