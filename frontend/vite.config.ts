fbe635714b13c0340617c986fba79a99f5434daf
matheusbat28 <matheusarrow28@gmail.com>
2024-06-12 00:17:43 -0300

    docker deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
+  preview: {
+    port: 3000,
+    strictPort: true,
+  },
+  server: {
+    port: 3000,
+    host: true,
+    watch: {
+      usePolling: true,
+    },
+  },
 })
